f30e783e06a7824a9fd1865ac089e006f4d7b922
Duc Nguyen <minhducng93@gmail.com>
2017-10-11 23:38:48 -0400

    Add User, Category, Event, EventType model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Event;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $all_events = Event::all();
         return view('home');
     }
 }
--- app/User.php
+++ app/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = "users";
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
