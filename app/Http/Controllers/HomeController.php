534bddd7ef3bb4f48f18bcc2aa573738c5ba0a41
Duc Nguyen <minhducng93@gmail.com>
2017-10-13 11:03:48 -0400

    Add view view-event

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Event;
+use App\EventType;
+use App\Category;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -26,6 +29,41 @@ class HomeController extends Controller
     public function index()
     {
         $all_events = Event::all();
-        return view('home');
+        $all_events_types = EventType::all();
+        $all_categories = Category::all();
+        $types = [];
+        $categories = [];
+        foreach ($all_events_types as $t) {
+            array_push($types, ['text' => $t->name, 'url' => "event-type/" . $t->name]);
+        }
+        foreach ($all_categories as $c) {
+            array_push($categories, ['text' => $c->name, 'url' => "category/" . $c->name]);
+        }
+        $this->generateData();
+        return view('home', ['event' => $all_events, 'event_type' => $types, 'category' => $categories]);
+    }
+
+    public function generateData () {
+
+        $event = new Event();
+        $event->location = "";
+        $event->title = "";
+        $event->startdate = "";
+        $event->enddate = "";
+        $event->starttime = "";
+        $event->endtime = "";
+        $event->description = "";
+        $event->user_id = "";
+        $event->organizer_description = "";
+        $event->event_type_id = "";
+        $event->background_photo = "";
+        $event->template = "";
+        $event->category_id = "";
+        $event->url = "";
+        $event->registered_amount = "";
+        $event->capacity = "";
+        $event->code = "";
+        $event->price = "";
+        $event->save();
     }
 }
