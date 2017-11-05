83b962bd7fb07fef52a27ee2cafc71681819d85f
Duc Nguyen <minhducng93@gmail.com>
2017-11-05 15:43:59 -0500

    Add name in navbar when user logged in

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<meta name="_token" content="{{ csrf_token() }}" />
 <head>
     <link href='https://fonts.googleapis.com/css?family=Roboto|Material+Icons' rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
@@ -13,32 +15,46 @@
     <style type="text/css">
         a {
             text-decoration:none;
+            color:black;
         }
         #title{
             color:red;
             font-family: 'Kaushan Script', cursive;
             font-size:30px;
         }
+
     </style>
     @yield('style')
 </head>
 <body>
-<div id="app">
-    <v-app>
-        <v-toolbar class="white" style="z-index:2;">
-            <v-toolbar-title><a href="/home" id="title">Plantastic</a></v-toolbar-title>
-            <v-spacer></v-spacer>
-            <form method="post">
-                <v-text-field style="width:150%;" append-icon="search" v-bind:append-icon-cb="submit" v-model="search" hide-details single-line placeholder="Enter any search events" required></v-text-field>
-            </form>
-            <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-sm-and-down" v-for="button in buttons" :key="button.text">
-                <v-btn flat :href=button.url class="button">@{{ button.text }}</v-btn>
-            </v-toolbar-items>
-            <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn flat href="/login">Create Event</v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
+<div>
+    <v-toolbar class="white" style="z-index:2;" id="navbar">
+        <v-toolbar-title><a href="/home" id="title">Plantastic</a></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <form method="post" action="/search">
+            <v-text-field style="width:150%;" append-icon="search" v-bind:append-icon-cb="submit" v-model="search" hide-details single-line placeholder="Enter any search events" required></v-text-field>
+        </form>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down" v-for="button in buttons" :key="button.text">
+            <v-btn flat :href=button.url class="button">@{{ button.text }}</v-btn>
+        </v-toolbar-items>
+        <v-toolbar-items>
+            <v-menu open-on-hover offset-y>
+                <v-btn flat slot="activator" v-if="user_login">@{{ user_login.firstname }}</v-btn>
+                <v-list class="ma-0 pa-0 grey lighten-4">
+                    <v-list-tile v-for="(dropdown,c) in dropdowns" :key="c" @click="redirect(dropdown.url)">
+                        <v-list-tile-title>@{{ dropdown.text }}</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat href="/login">Create Event</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+
+    <v-app id="app">
         @yield('body')
         <v-footer class="grey darken-4 pa-3" id="footer" style="z-index:2;">
             <v-spacer></v-spacer>
@@ -48,6 +64,54 @@
 </div>
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 <script src="https://unpkg.com/vuetify/dist/vuetify.js"></script>
+<script>
+    //axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    var user_login = <?php echo json_encode($user_login); ?>;
+
+    new Vue({
+        el: '#navbar',
+        data: {
+            search:'',
+            showUserMenu: false,
+            buttons: [
+                {
+                    text: 'Home',
+                    url: '/home'
+                },
+                {
+                    text: 'Register',
+                    url: '/register'
+                }],
+            user_login: user_login,
+            dropdowns: [
+                {
+                    text: 'User Profile',
+                    url: '/user/profile/'
+                },
+                {
+                    text: 'Manage Event',
+                    url: '/user/manage-event/'
+                }]
+        },
+        methods: {
+            submit: function() {
+                axios.post('/api/submit',{
+                    search:this.search
+
+                }).then((result) => {
+                    if (result) {
+
+                    } else {
+
+                    }
+                })
+            },
+            redirect: function(link){
+                window.location.href=link
+            }
+        }
+    })
+</script>
 @yield('script')
 </body>
 </html>
