d454b42eeceeab6ac797e59f5e26364cc0ec5122
Duc Nguyen <crkacer@users.noreply.github.com>
2018-01-11 23:37:13 -0500

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -53,37 +53,6 @@ class LoginController extends Controller
 
     public function getRegister() {
 
-        if (\Request::input('email') && \Request::input('password') ) {
-            $userData = [
-                'fname' => \Request::input('first_name'),
-                'lname' => \Request::input('last_name'),
-                'email' => \Request::input('email'),
-                'password' => \Request::input('password')
-            ];
-            // If user does not exist then allow to create
-            if (User::where('email', '=', $userData['username'])->exists()) {
-                // handle user already existed
-
-            } else {
-                // if not then processing register
-                $user = new User;
-                $user->name = $userData['fname']." ". $userData['lname'];
-                $user->firstname = $userData['fname'];
-                $user->lastname = $userData['lname'];
-                $user->password = Hash::make($userData['password']);
-                $user->email = $userData['username'];
-                $user->save();
-
-                $login_path = \Request::input('login_path') ?: Cookie::get('login_path');
-                if (empty($login_path)) {
-                    return redirect('/');
-                } else {
-                    $cookie = Cookie::forget('login_path');
-                    return redirect($login_path)->withCookie($cookie);
-                }
-            }
-        }
-
         return View::make('register', [
             'user_login' => Auth::user()
         ]);
